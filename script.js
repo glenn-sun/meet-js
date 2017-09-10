4313826fe1d92de2a19205d830a36b88eba8ddf9
VineethVajipey <vineeth.vajipey@gmail.com>
2017-09-09 22:51:30 -0400

    post and get functions, API version

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+var userFriends
+
+function newUserFirebase() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
-  console.log('statusChangeCallback');
-  console.log(response);
   // The response object is returned with a status field that lets the
   // app know the current login status of the person.
   // Full docs on the response object can be found in the documentation
@@ -16,39 +25,30 @@ function statusChangeCallback(response) {
   }
 }
 
-// This function is called when someone finishes with the Login
-// Button.  See the onlogin handler attached to it in the sample
-// code below.
-function checkLoginState() {
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1186337974831392',
+    cookie     : true,  // enable cookies to allow the server to access
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.10' // use graph api version 2.8
+  });
+
+  // Now that we've initialized the JavaScript SDK, we call
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
   });
-}
-
-window.fbAsyncInit = function() {
-FB.init({
-  appId      : '1186337974831392',
-  cookie     : true,  // enable cookies to allow the server to access
-                      // the session
-  xfbml      : true,  // parse social plugins on this page
-  version    : 'v2.8' // use graph api version 2.8
-});
-
-// Now that we've initialized the JavaScript SDK, we call
-// FB.getLoginStatus().  This function gets the state of the
-// person visiting this page and can return one of three states to
-// the callback you provide.  They can be:
-//
-// 1. Logged into your app ('connected')
-// 2. Logged into Facebook, but not your app ('not_authorized')
-// 3. Not logged into Facebook and can't tell if they are logged into
-//    your app or not.
-//
-// These three cases are handled in the callback function.
-
-FB.getLoginStatus(function(response) {
-  statusChangeCallback(response);
-});
 
 };
 
@@ -70,8 +70,52 @@ function testAPI() {
     console.log('Successful login for: ' + response.name);
     document.getElementById('status').innerHTML =
       'Thanks for logging in, ' + response.name + '!';
+    
+    //DEFINE USER FRIENDSSSSSSSSZZZZZ
+    
+    if (!checkIfUserExists(reposnse.id)){
+      postName(resposnse.name, userId)
+      postFriendsList(userFriends, userId)
+      postId(userName, response.id)
+    }
+  });
+}
+//FIREBASE
+
+var database = firebase.database
+
+//Check if new user
+
+function checkIfUserExists(id) {
+  database.ref('/idList').once('value').then(function(snapshot) {
+    return snapshot.val.includes(id);
   });
 }
 
+// Post data to Firebase
+
+function postFriendsList(friendsList, id) {
+    database.ref('friendsList').set({
+    id: friendsList
+  });
+}
+
+function postName(name, id) {
+  database.ref('nam').set({
+    id: name
+  });
+}
+
+function postId(id) {
+  database.ref('idList')({
+    id
+  })
+}
+
+
 // Get data from Firebase
 
+function getFriendsList(id) {
+    
+    document.getElementById(id)
+}
